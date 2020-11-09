c03051d2cc0e77569355b37c61a74341c252136a
kreid333 <kai.j.reid@gmail.com>
2020-11-09 11:05:01 -0500

    basic login page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
+import "./App.css";
 
 function App() {
   useEffect(() => {
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import "./Login.css";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
+  return (
+    <div id="loginCenter">
+      <h1 className="text-center mt-3">Login</h1>
+      <div className="text-center">
+        <form>
+          <label htmlFor="emailAddress" className="mt-3">
+            Email Address
+          </label>
+          <br />
+          <input type="text" name="emailAddress" id="emailAddress" />
+          <br />
+          <label htmlFor="password" className="mt-3">
+            Password
+          </label>
+          <br />
+          <input type="text" name="password" id="password" />
+          <br />
+          <button className="btn button mt-3">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
