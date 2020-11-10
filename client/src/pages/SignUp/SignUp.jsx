d8fc988a8bd60ca8ab7a32cc05072f30123f86f9
Patrick Hannan <patrick@hannan.com>
2020-11-10 11:02:17 -0500

    Added base framework for submit route

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
+
+    const [gender, setGender] = useState("");
+    const [fullName, setFullName] = useState("");
+    const [emailAddress, setEmailAddress] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (gender, fullName, emailAddress, password) => {
+        e.preventDefault();
+        axios
+          .post("/api/users", { gender, fullName, emailAddress, password })
+          .then((response) => {
+            console.log(response.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+
     return (
     <div className="container">
         <div className="row">
@@ -12,7 +30,9 @@ const SignUp = () => {
         </div>
         <div className="row">
             <div className="col-sm-12 mt-3 text-center">
-                <form>
+                <form onSubmit={(e) => {
+                    handleSubmit(e, gender, fullName, emailAddress, password);
+                }}>
                     <label for="userRole">
                         Who are you?
                     </label>
@@ -26,35 +46,48 @@ const SignUp = () => {
                         Gender
                     </label>
                     <br />
-                    <select id="genderFormControl">
+                    <select 
+                        id="genderFormControl" 
+                        value={gender}
+                        onChange={(e) => setGender(e.target.value)}>
                         <option>Male</option>
                         <option>Female</option>
                         <option>Other</option>
                     </select>
                     <br />
-                    <label for="firstName" className="mt-3">
-                        First Name
+                    <label for="fullName" className="mt-3">
+                        Full Name
                     </label>
                     <br />
-                    <input type="text" id="firstNameFormControl"></input>
-                    <br />
-                    <label for="lastName" className="mt-3">
-                        Last Name
-                    </label>
-                    <br />
-                    <input type="text" id="lastNameFormControl"></input>
+                    <input 
+                        type="text" 
+                        id="fullNameFormControl"
+                        value={fullName}
+                        onChange={(e) => setFullName(e.target.value)}
+                        />
                     <br />
                     <label for="emailAddress" className="mt-3">
                         Email address
                     </label>
                     <br />
-                    <input type="email" id="emailFormControl" placeholder="user@email.com"></input>
+                    <input 
+                        type="email" 
+                        id="emailFormControl" 
+                        placeholder="user@email.com" 
+                        value={emailAddress}
+                        onChange={(e) => setEmailAddress(e.target.value)}
+                    />
                     <br />
                     <label for="password" className="mt-3">
                         Password
                     </label>
                     <br />
-                    <input type="password" id="passwordInput"></input>
+                    <input 
+                        type="password" 
+                        id="passwordFormControl" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                     <br />
                 <Link to="/dashboard">
                     <button type="submit" className="btn button mt-3">Sign Up!</button>
