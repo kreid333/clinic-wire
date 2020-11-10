b1ad63e5a5b702e106393c217badd63826d5a7b4
Patrick Hannan <patrick@hannan.com>
2020-11-10 10:16:54 -0500

    Built sign up page

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
@@ -10,45 +11,57 @@ const SignUp = () => {
             </div>
         </div>
         <div className="row">
-            <div className="col-sm-12 bg-image mt-3 text-center">
+            <div className="col-sm-12 mt-3 text-center">
                 <form>
-                <div class="form-group">
-                    <label for="exampleFormControlSelect1">Who are you?</label>
-                    <select class="form-control" id="roleFormControl">
+                    <label for="userRole">
+                        Who are you?
+                    </label>
+                    <br />
+                    <select id="roleFormControl">
                         <option>Patient</option>
                         <option>Doctor</option>
                     </select>
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Email address</label>
-                    <input type="email" class="form-control" id="emailFormControl" placeholder="name@example.com"></input>
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">First Name</label>
-                    <input type="name" class="form-control" id="firstNameFormControl"></input>
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Last Name</label>
-                    <input type="name" class="form-control" id="lastNameFormControl"></input>
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlSelect1">Gender</label>
-                    <select class="form-control" id="genderFormControl">
+                    <br />
+                    <label for="gender" className="mt-3">
+                        Gender
+                    </label>
+                    <br />
+                    <select id="genderFormControl">
                         <option>Male</option>
                         <option>Female</option>
                         <option>Other</option>
                     </select>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="passwordInput"></input>
-                </div>
+                    <br />
+                    <label for="firstName" className="mt-3">
+                        First Name
+                    </label>
+                    <br />
+                    <input type="text" id="firstNameFormControl"></input>
+                    <br />
+                    <label for="lastName" className="mt-3">
+                        Last Name
+                    </label>
+                    <br />
+                    <input type="text" id="lastNameFormControl"></input>
+                    <br />
+                    <label for="emailAddress" className="mt-3">
+                        Email address
+                    </label>
+                    <br />
+                    <input type="email" id="emailFormControl" placeholder="user@email.com"></input>
+                    <br />
+                    <label for="password" className="mt-3">
+                        Password
+                    </label>
+                    <br />
+                    <input type="password" id="passwordInput"></input>
+                    <br />
                 <Link to="/dashboard">
-                    <button type="submit" class="btn" id="signUpConfirmBtn">Sign Up!</button>
+                    <button type="submit" className="btn button mt-3">Sign Up!</button>
                 </Link>
                 </form>
-                <Link to="/homepage">
-                <button class="btn" id="homeBackBtn">
+                <Link to="/">
+                <button className="btn button mt-3">
                 Back
                 </button>
                 </Link>
